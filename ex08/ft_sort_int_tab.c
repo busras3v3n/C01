8da44cb2d8940374172ac74a2aa264d8dcefbe04
Büşra Seven <152426280+busras3v3n@users.noreply.github.com>
2024-07-13 18:44:56 +0300

    8

--- ex08/ft_sort_int_tab.c
+++ ex08/ft_sort_int_tab.c
@@ -3,13 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: busseven <busras3v3n@proton.me>            +#+  +:+       +#+        */
+/*   By: busseven <busseven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 16:04:23 by busseven          #+#    #+#             */
-/*   Updated: 2024/07/11 16:34:13 by busseven         ###   ########.fr       */
+/*   Updated: 2024/07/13 18:26:33 by busseven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_sort_int_tab(int *tab, int size)
 {
+	int	i;
+	int	j;
+	int	temp;
+
+	i = 0;
+	j = 0;
+	while (i < size - 1)
+	{
+		j = 0;
+		while (j < size - i - 1)
+		{
+			if (tab[j] > tab[j + 1])
+			{
+				temp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
 }
